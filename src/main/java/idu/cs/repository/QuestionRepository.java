33c657eb697b5c4e07a003380ca855076d902d0a
user <user@IN302>
2019-06-05 18:01:57 +0900

    list.html 추가

--- src/main/java/idu/cs/repository/QuestionRepository.java
+++ src/main/java/idu/cs/repository/QuestionRepository.java
@@ -6,20 +6,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import idu.cs.entity.QuestionEntity;
 import idu.cs.entity.UserEntity;
 
 @Repository
 public interface QuestionRepository 
-	extends JpaRepository<UserEntity, Long> {
+	extends JpaRepository<QuestionEntity, Long> {
 	//findById, save, delete
 	
-	// 아래 메소드들은 선언해야 구현됨
-	// find - select문, by - Where, OrderBy - order by, ASC, DESC
-	List<UserEntity> findByName(String name);
-	
-	//id 내림차순
-	//List<UserEntity> findByNameOrderByIdDESC(String name);
-	List<UserEntity> findByCompany(String company);
-	
-	UserEntity findByUserid(String userId);
+
+	QuestionEntity findByWriter(String writer);
 }
